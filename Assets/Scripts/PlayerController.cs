fc215a491ef1e04cb202c4fa287967f196764b23
AlbertAzaryev <freesenser5@yahoo.com>
2025-09-24 22:34:09 +0300

    first commit limud1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Vector2 moveInput;
+    public float moveSpeed = 5f;
+    public InputActionReference moveAction;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        if (moveAction != null && moveAction.action != null)
+        {
+            moveAction.action.performed += OnMovePerformed;
+            moveAction.action.canceled += OnMoveCanceled;
+            moveAction.action.Enable();
+        }
+    }
+
+    public void OnMovePerformed(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnMoveCanceled(InputAction.CallbackContext context)
+    {
+        moveInput = Vector2.zero;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 move = new Vector3(moveInput.x, 0f, moveInput.y);
+        Vector3 velocity = rb.linearVelocity;
+        velocity.x = move.x * moveSpeed;
+        velocity.z = move.z * moveSpeed;
+        rb.linearVelocity = velocity;
+    }
+
+    void OnDisable()
+    {
+        if (moveAction != null && moveAction.action != null)
+        {
+            moveAction.action.performed -= OnMovePerformed;
+            moveAction.action.canceled -= OnMoveCanceled;
+            moveAction.action.Disable();
+        }
+    }
+}
