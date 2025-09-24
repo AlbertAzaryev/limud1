1a1e321b2e6ca7841477cb832f353582c30c90f3
AlbertAzaryev <freesenser5@yahoo.com>
2025-09-24 23:14:13 +0300

    jump

---
+++ Assets/Scripts/JumpFeature.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class JumpFeature : MonoBehaviour
+{
+    public float jumpForce = 5f;
+    private Rigidbody rb;
+    private bool isGrounded = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
